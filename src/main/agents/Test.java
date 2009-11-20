fe5242ca10827c766e75342af7d20be7942046b2
rogeliorv <rogeliorv@1da094c4-c6ba-11de-8bd7-25a952cf5249>
2009-11-20 09:35:18 +0000

    JADE Contract Net example with simple Agents and behaviors

--- src/main/agents/Test.java
+++
@@ -1,9 +0,0 @@
-package main.agents;
-import jade.core.Agent;
-
-public class Test extends Agent {
-	protected void setup() {
-		// Printout a welcome message
-		System.out.println("Hello! Buyer-agent " +getAID().getName()+ " is ready.");
-	}
-}
