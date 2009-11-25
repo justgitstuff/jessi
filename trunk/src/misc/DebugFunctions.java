b644e85f13d2fdb1f2043511c80d7fe1ee41b9bc
rogeliorv <rogeliorv@1da094c4-c6ba-11de-8bd7-25a952cf5249>
2009-11-25 02:16:44 +0000

    Added debug functions and added logging to the connection factory file.

--- trunk/src/misc/DebugFunctions.java
+++ trunk/src/misc/DebugFunctions.java
@@ -5,29 +5,44 @@ import jade.core.Agent;
 
 public final class DebugFunctions {
 	
-	/** Debug flag */
+	// Debug flag 
 	private static final boolean DEBUG = true;
-	/** Output to stdout and stderr */
+	// Output to stdout
 	private static final PrintWriter PRINT_ERROR = new PrintWriter(System.err);
+	// Output to stderr
 	private static final PrintWriter PRINT_MSG = new PrintWriter(System.out);
 	
-	/** Log function */
+	/** Basic log function, used by all the other log functions */
 	private static void log(String who, String message, PrintWriter p) {
-		p.println("Agent " + who + ": " +  message);
-		p.flush();
-	}
-	/** Log function */
-	private static void log(Agent agent, String message, boolean error) {
 		if(DEBUG) {
-			log(agent.getLocalName(), message, error ? PRINT_ERROR : PRINT_MSG);
+			p.println("Agent " + who + ": " +  message);
+			p.flush();
 		}
 	}
+	
 	/** Log function */
+	private static void log(Agent agent, String message, boolean error) {
+		String name = agent != null ? agent.getLocalName() : "";
+		log(name, message, error ? PRINT_ERROR : PRINT_MSG);
+		
+	}
+	/** Message log with agent */
 	public static void log(Agent agent, String message) {
 		log(agent, message, false);
 	}
 	
+	/** Message log without agent */
+	public static void log(String message) {
+		log(null, message);
+	}
+	
+	/** Error log with agent */
 	public static void logError(Agent agent, String message) {
 		log(agent, message, true);
+	}
+	
+	/** Error log without agent */
+	public static void logError(String message) {
+		logError(null, message);
 	}
 }
