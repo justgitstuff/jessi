006c4a6eb1b77eee6a2f179edc130568a4595ec9
rogeliorv <rogeliorv@1da094c4-c6ba-11de-8bd7-25a952cf5249>
2009-11-25 02:16:44 +0000

    Added debug functions and added logging to the connection factory file.

--- src/main/agents/ConnectionFactory.java
+++ src/main/agents/ConnectionFactory.java
@@ -7,27 +7,32 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import static misc.DebugFunctions.*;
 
+/** Factory that generates SQL connections to the database. */
 public class ConnectionFactory {
 	
-	String confFile = "conf/conf.properties";
-	Properties properties;
+	// Configuration file 
+	private String confFile = "conf/conf.properties";
+	// This is where the properties in the configuration file are read
+	private Properties properties;
 	
+	/** Load the properties */
 	public ConnectionFactory(){
 		properties = new Properties();
 		loadProperties();
 	}
 	
+	/** Generate a new connection to the database */
+	// TODO: Shouldn't this method use the other more general method ? 
 	public Connection getConnection(){
 		Connection connection = null;
 
 	    try {
 	        String driverName = properties.getProperty("db.1.driver"); // MySQL MM JDBC driver
-	        System.out.println("driver: " + driverName);
-	        Class.forName(driverName);
-	    
+	        log("Driver name for the DB is: " + driverName);
+	        Class.forName(driverName);	    
 	        // Create a connection to the database
-
 	        String url = properties.getProperty("db.1.url"); // a JDBC url
 	        String username = properties.getProperty("db.1.username");
 	        String password = properties.getProperty("db.1.password");
@@ -35,18 +40,22 @@ public class ConnectionFactory {
 	        
 	        return connection;
 	    } catch (ClassNotFoundException e) {
-	        e.printStackTrace();
+	        logError("Error generating the database connection.");
+	    	e.printStackTrace();
 	    } catch (SQLException e) {
+	    	logError("Error generating the database connection.");
 	    	e.printStackTrace();
 	    }
 		return null;
 	}
+	
+	/** Generate a new connection to the database given the database name */
 	public Connection getConnection(String nombreDB){
 		Connection connection = null;
 
 	    try {
 	        String driverName = properties.getProperty("db.1.driver"); // MySQL MM JDBC driver
-	        System.out.println("driver: " + driverName);
+	        log("Driver name for the DB is: " + driverName);
 	        Class.forName(driverName);
 	    
 	        // Create a connection to the database
@@ -58,8 +67,10 @@ public class ConnectionFactory {
 	        
 	        return connection;
 	    } catch (ClassNotFoundException e) {
-	        e.printStackTrace();
+	    	logError("Error generating the database connection.");
+	    	e.printStackTrace();
 	    } catch (SQLException e) {
+	    	logError("Error generating the database connection.");
 	    	e.printStackTrace();
 	    }
 		return null;
@@ -70,10 +81,10 @@ public class ConnectionFactory {
 			properties.load(new FileInputStream(confFile));
 			System.out.println("Se cargo bien");
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			logError("Properties file for the DB connection not found.");
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			logError("Error getting the properties file for the DB connection");
 			e.printStackTrace();
 		}
 	}
