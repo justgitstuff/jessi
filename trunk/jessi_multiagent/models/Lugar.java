6c804b99305d7293250ca54061169b824837332b
rogeliorv <rogeliorv@1da094c4-c6ba-11de-8bd7-25a952cf5249>
2009-11-26 04:45:25 +0000

    Models finished.

--- trunk/jessi_multiagent/models/Lugar.java
+++ trunk/jessi_multiagent/models/Lugar.java
@@ -7,33 +7,95 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 
 public class Lugar extends Model {
-	
+
 	private int id;
 	private int capacidad;
 	private String tipo;
 	private String codigo;
-	private LinkedList<Hora> horarios;
-	
-	protected Lugar(int id, int capacidad, String tipo, String codigo) {
+	private LinkedHashSet<Horario> horariosDisp;
+	private static final LinkedHashSet<Horario> horariosDispGeneral = generaHorario();
+
+	protected Lugar(int id, int capacidad, String tipo, String codigo)
+			throws SQLException {
 		this.id = id;
 		this.capacidad = capacidad;
 		this.tipo = tipo;
 		this.codigo = codigo;
+		this.horariosDisp = getHorario();
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Lugar(" + id + "," + capacidad + "," + tipo + "," + codigo + ")";
+		return "Lugar(" + id + "," + capacidad + "," + tipo + "," + codigo
+				+ horariosDisp + ")";
 	}
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public LinkedHashSet<Horario> getHorariosDisp() {
+		return horariosDisp;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		return this.id == ((Lugar) obj).id;
 	}
-	
+
+	private LinkedHashSet<Horario> getHorario() throws SQLException {
+		if (horariosDisp == null) {
+			horariosDisp = new LinkedHashSet<Horario>();
+			for (Horario h : horariosDispGeneral) {
+				horariosDisp.add(new Horario(h.getId(), h.getdiaId(), h
+						.getHoraInicio(), h.getHoraFin()));
+			}
+		}
+		return horariosDisp;
+	}
+
+	// Funcion que regresa regresa un conjunto con todos los horarios indicados
+	// en la base de datos.
+	private static LinkedHashSet<Horario> generaHorario() {
+
+		LinkedHashSet<Horario> horarioSet = new LinkedHashSet<Horario>();
+		String query = "select * from horarios_disp";
+		try {
+			Connection c = Model.getDBConnection();
+			PreparedStatement s = c.prepareStatement(query);
+			ResultSet result = s.executeQuery();
+
+			while (result.next()) {
+				int id = result.getInt("Horario_Disp_Id");
+				int diaId = result.getInt("Dia_Id");
+				int horaInicio = result.getInt("Hora_Inicio");
+				int horaFin = result.getInt("Hora_Fin");
+				horarioSet.add(new Horario(id, diaId, horaInicio, horaFin));
+			}
+		} catch (SQLException e) {
+			logError("SQL Statement failed: " + query);
+			e.printStackTrace();
+			horarioSet = null;
+		}
+		return horarioSet;
+	}
+
 	public static Lugar createLugarFromId(int id) throws SQLException {
 		String query = "select * from lugar where Lugar_Id = " + id + ";";
 		Lugar lugar = null;
@@ -47,12 +109,45 @@ public class Lugar extends Model {
 			String codigo = result.getString("Codigo");
 			lugar = new Lugar(id, capacidad, tipo, codigo);
 			log("Loading " + lugar.toString());
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			logError("SQL Statement failed: " + query);
 			e.printStackTrace();
 			throw new SQLException(e);
 		}
 		return lugar;
+	}
+	
+	public static LinkedList<Lugar> createAll() throws SQLException {
+
+		LinkedList<Lugar> professors = new LinkedList<Lugar>();
+		String query = "select * from lugar";
+
+		try {
+			Connection c = Model.getDBConnection();
+			PreparedStatement s = c.prepareStatement(query);
+			ResultSet result = s.executeQuery();
+
+			while (result.next()) {
+				int id = result.getInt("Lugar_Id");
+				int capacidad = result.getInt("Capacidad");
+				String tipo = result.getString("Tipo");
+				String codigo = result.getString("Codigo");
+				Lugar l = new Lugar(id, capacidad, tipo, codigo);
+				professors.add(l);
+				log("Loading " + l.toString());
+			}
+
+		} catch (SQLException e) {
+			logError("SQL Statement failed: " + query);
+			e.printStackTrace();
+			throw new SQLException(e);
+		}
+		return professors;
+	}
+
+	public static void main(String[] args) throws Exception {
+		for (int i = 1; i < 100; i++) {
+			createLugarFromId(i);
+		}
 	}
 }
--- trunk/jessi_multiagent/models/Profesor.java
+++ trunk/jessi_multiagent/models/Profesor.java
@@ -6,79 +6,80 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
-import java.util.TreeSet;
 
 import static misc.DebugFunctions.*;
 
 /**
- * Read-only model for the professor class.
- * Future Change: Change this to use Hibernate.
+ * Read-only model for the professor class. Future Change: Change this to use
+ * Hibernate.
  */
 public class Profesor extends Model {
-	
+
 	private int id;
 	private String nombre;
 	private String tipo;
-	private LinkedHashSet<Horario> horarios;
-	
+	private LinkedHashSet<Horario> horariosDisp;
+
 	protected Profesor(int id, String nombre, String tipo) throws SQLException {
 		this.id = id;
 		this.nombre = nombre;
 		this.tipo = tipo;
-		this.horarios = getHorario();
+		this.horariosDisp = getHorario();
 	}
-		
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public String getTipo() {
 		return tipo;
 	}
-	
-	private LinkedHashSet<Horario> getHorario() throws SQLException{
-		if(horarios == null) {
-			horarios = new LinkedHashSet<Horario>();
-			String query = "select * from horarios_disp " +
-						   "where Horario_Disp_Id in " +
-						   "(select Horario_Disp_Id from profesor_horario " +
-						   "where profesor_id = " + id +")";
+
+	public LinkedHashSet<Horario> getHorario() throws SQLException {
+		if (horariosDisp == null) {
+			horariosDisp = new LinkedHashSet<Horario>();
+			String query = "select * from horarios_disp "
+					+ "where Horario_Disp_Id in "
+					+ "(select Horario_Disp_Id from profesor_horario "
+					+ "where profesor_id = " + id + ")";
 			try {
 				Connection c = Model.getDBConnection();
 				PreparedStatement s = c.prepareStatement(query);
 				ResultSet result = s.executeQuery();
-				
-				while(result.next()) {
+
+				while (result.next()) {
 					int id = result.getInt("Horario_Disp_Id");
 					int diaId = result.getInt("Dia_Id");
 					int horaInicio = result.getInt("Hora_Inicio");
 					int horaFin = result.getInt("Hora_Fin");
-					horarios.add(new Horario(id, diaId, horaInicio, horaFin));
+					horariosDisp
+							.add(new Horario(id, diaId, horaInicio, horaFin));
 				}
 			} catch (SQLException e) {
 				logError("SQL Statement failed: " + query);
 				e.printStackTrace();
 				throw new SQLException(e);
-			}		
+			}
 		}
-		return horarios;
+		return horariosDisp;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Profesor("+id+","+nombre+","+tipo+","+horarios+")";
+		return "Profesor(" + id + "," + nombre + "," + tipo + ","
+				+ horariosDisp + ")";
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		return this.id == ((Profesor) obj).id;
 	}
-	
-	/** Load a professor from the database given his id*/
+
+	/** Load a professor from the database given his id */
 	public static Profesor createProfesorFromId(int id) throws SQLException {
 		String query = "select * from profesor where Profesor_Id = " + id + ";";
 		Profesor prof = null;
@@ -88,49 +89,42 @@ public class Profesor extends Model {
 			ResultSet result = s.executeQuery();
 			result.first();
 			String nombre = result.getString("Profesor_Nombre");
-			String tipo = result.getString("Profesor_Tipo");				
+			String tipo = result.getString("Profesor_Tipo");
 			prof = new Profesor(id, nombre, tipo);
 			log("Loading " + prof.toString());
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			logError("SQL Statement failed: " + query);
 			e.printStackTrace();
 			throw new SQLException(e);
 		}
 		return prof;
 	}
-	
+
 	/** Load all the professors from the database table */
-	public static LinkedList<Profesor> loadProfessors() throws SQLException{
-		
+	public static LinkedList<Profesor> createAll() throws SQLException {
+
 		LinkedList<Profesor> professors = new LinkedList<Profesor>();
 		String query = "select * from profesor";
-		
+
 		try {
 			Connection c = Model.getDBConnection();
 			PreparedStatement s = c.prepareStatement(query);
 			ResultSet result = s.executeQuery();
-			
-			while(result.next()) {
+
+			while (result.next()) {
 				int id = result.getInt("Profesor_Id");
 				String nombre = result.getString("Profesor_Nombre");
-				String tipo = result.getString("Profesor_Tipo");				
+				String tipo = result.getString("Profesor_Tipo");
 				Profesor p = new Profesor(id, nombre, tipo);
 				professors.add(p);
 				log("Loading " + p.toString());
 			}
-			
+
 		} catch (SQLException e) {
 			logError("SQL Statement failed: " + query);
 			e.printStackTrace();
-			throw new SQLException(e); 
-		}		
-		return professors;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		for(int i = 1; i < 10; i++) {
-			createProfesorFromId(i);
+			throw new SQLException(e);
 		}
+		return professors;
 	}
 }
